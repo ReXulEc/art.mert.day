9261609e5821072baf0fba2440f447d1cee6ecf7
Mert Dogu <mertdoguonder@protonmail.com>
2026-01-15 17:31:55 +0300

    adapter issue

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-    kit: {
-        adapter: adapter(),
-        prerender: {
-            default: true
-        }
-    }
+export default {
+  kit: {
+    adapter: adapter()
+  }
 };
-
-export default config;
